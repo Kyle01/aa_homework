d849e66c2ab481813c92360c20f77033c552db6d
Kyle McV <kylemcv@gmail.com>
2018-06-26 21:33:29 -0700

    finish rspecs

--- W2D3/spec/dessert_spec.rb
+++ W2D3/spec/dessert_spec.rb
@@ -6,8 +6,8 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
   subject(:brownie) {Dessert.new("brownie", 10, :chef)}
+  let(:chef) { double("chef", name: "Kyle") }
 
   describe "#initialize" do
     it "sets a type" do
@@ -28,24 +28,49 @@ describe Dessert do
 
   describe "#add_ingredient" do
     it "adds an ingredient to the ingredients array" do
-      
+      brownie.add_ingredient("nuts")
+      expect(brownie.ingredients).to include("nuts")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["eggs, chocolate, nuts, milk"]
+
+      ingredients.each do |ingredient|
+        brownie.add_ingredient(ingredient)
+      end
+
+      expect(brownie.ingredients).to eq(ingredients)
+      brownie.mix!
+      expect(brownie.ingredients).not_to eq(ingredients)
+      expect(brownie.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      brownie.eat(5)
+      expect(brownie.quantity).to eq(5)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { brownie.eat(100)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Kyle the Great Baker")
+      expect(brownie.serve).to eq("Chef Kyle the Great Baker has made 100 brownies!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(brownie)
+      brownie.make_more
+    end
   end
 end
